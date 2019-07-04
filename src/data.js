473a85f501fdc32faa43f39bcd66dbccc7975fc1
Lichadri <lichibautista@gmail.com>
2019-07-04 12:08:26 -0500

    cambios segunda vista

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
 const contenedor = document.getElementById('contenedor');
+const atrapados = document.getElementById('catch');
 const pokeData = () => {
   for (let i = 0; i < POKEMON.pokemon.length; i++) {
     const num = POKEMON.pokemon[i].num;
     const nombre = POKEMON.pokemon[i].name;
     const imagen = POKEMON.pokemon[i].img;
+    const estado = POKEMON.pokemon[i].multipliers;
 
     contenedor.innerHTML += `<div class='poke' id="poker">
     <p> ${num} </p>
@@ -11,4 +13,5 @@ const pokeData = () => {
     <img src=" ${imagen}"/>
     </div>`;
   }
-}
+};
+
