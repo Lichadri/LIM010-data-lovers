6ed3aaf11f15378d454353e3d2af9a1ffb8fa4c9
HIDUBA <lescatap@gmail.com>
2019-07-05 00:59:07 -0500

    Ventana emergente muestra caracteristicas de pokemones

--- src/data.js
+++ src/data.js
@@ -1,47 +1,66 @@
-const filterOrder = document.getElementById('filtro-orden');
-const filterType = document.getElementById('filtro-tipo');
-//funcion que espera que se seleccione algun filtro
-filterOrder.addEventListener('change', () => {
-  ordenar();
-});
-filterType.addEventListener('change', () => {
-  ordenar();
-});
-
-//funcion ordenar
-const ordenar = () => {
-  const pokemonesFiltrados = POKEMON.pokemon;
-  mostrarPokemones(pokemonesFiltrados);
-}
+/* Asignar contenedor a variable */
 const contenedor = document.getElementById('contenedor');
+
+/* FUCNION DE MOSTRAR POKEMONES */
 const mostrarPokemones = (data) => {
   for (let i = 0; i < data.length; i++) {
     const num = data[i].num;
+    const id = data[i].id;
     const nombre = data[i].name;
     const imagen = data[i].img;
 
-    contenedor.innerHTML += `<div class='poke' id="poker">
+    contenedor.innerHTML += `<div class='poke' name='pokemon' id=${id}>
     <p> ${num} </p>
     <p> ${nombre} </p>
     <img src="${imagen}"/>
     </div>`;
   }
-}
 
-const atrapados=document.getElementById("contador");
+  // crear array con pokemones que han sido mostrados con variable pokemon dentro del contenedor
+  pokemon = document.getElementsByName('pokemon');
+
+
+  /* EVENTO PARA SABER CUAL SE HIZO click */
+  for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].getAttribute('name') === 'pokemon') {
+      /* si hace click */
+      pokemon[i].addEventListener('click', () => {
+        console.log(event);
+        // se obtine el id del padre de la imagen
+        // se quita 1 para que coincida con array
+        const numero = parseInt(event.target.parentElement.id) - 1 ;
+        // Mostrar modal
+        document.getElementById('my-modal').classList.remove('hide');
+        // Insertar datos en Modal
+        document.getElementById('modal-info').innerHTML = `
+        <img class="imagenModal" src="${POKEMON.pokemon[numero].img}"/>
+        <p> ${POKEMON.pokemon[numero].num}  ${POKEMON.pokemon[numero].name}</p>
+        <p>Peso: ${POKEMON.pokemon[numero].weight}   </p> 
+        <p>Altura: ${POKEMON.pokemon[numero].height}</p>    
+        <p>Tipo: ${POKEMON.pokemon[numero].type}</p>
+        `;
+      });
+    };
+  }
+};
+
+/* cerrar Modal */
+document.getElementById('close').addEventListener('click', () => {
+  document.getElementById('my-modal').classList.add('hide');
+});
+
+const atrapados = document.getElementById('contador');
 let contAtrapados = 0;
 let contNoAtrapados = 0;
 for (let i = 0; i < POKEMON.pokemon.length; i++) {
- if (POKEMON.pokemon[i].multipliers === null) {
-   contNoAtrapados ++;
- } else {
-   contAtrapados = 148 - contNoAtrapados;
- }
+  if (POKEMON.pokemon[i].multipliers === null) {
+    contNoAtrapados++;
+  } else {
+    contAtrapados = 148 - contNoAtrapados;
+  }
 
- atrapados.innerHTML = `<div>
+  atrapados.innerHTML = `<div>
      ${contAtrapados}    ${contNoAtrapados}
      </div>`;
 };
-
-
 
--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
 const claveIngresada = 'LABORATORIA';
 const btnIngresar = document.getElementById('btn-ingresar');
 btnIngresar.addEventListener('click', () => {
-    const usuario = document.getElementById('usuario').value;
-    const contraseña = document.getElementById('password').value;
-    if (usuario === claveIngresada && contraseña === claveIngresada) {
-        document.getElementById('login').classList.add('hide');
-        document.getElementById('vista1').classList.remove('hide');
-        ordenar(POKEMON.pokemon);
-    }
-
-
-    else {
-        document.getElementById('resultado').innerHTML = "Usuario o  contraseña incorrecta";
-        //hacer foco a la contraseña para que se quede el puntero fijo
-        document.getElementById('usuario').focus();
-
-    }
+  const usuario = document.getElementById('usuario').value;
+  const contraseña = document.getElementById('password').value;
+  if (usuario === claveIngresada && contraseña === claveIngresada) {
+    document.getElementById('login').classList.add('hide');
+    document.getElementById('vista1').classList.remove('hide');
+    mostrarPokemones(POKEMON.pokemon);
+  } else {
+    document.getElementById('resultado').innerHTML = 'Usuario o  contraseña incorrecta';
+    /* hacer foco a la contraseña para que se quede el puntero fijo*/
+    document.getElementById('usuario').focus();
+  }
 });
 
