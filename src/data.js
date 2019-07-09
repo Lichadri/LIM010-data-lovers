3e3ef6f1d29ca544e2e1104d49fc9668540ebc05
HIDUBA <lescatap@gmail.com>
2019-07-09 11:52:20 -0500

    Agregando filtro de ordenar A-Z, Z-A. pokemones que se muestran mas y menos :fire:

--- src/data.js
+++ src/data.js
@@ -1,58 +1,49 @@
-/* Asignar contenedor a variable */
-const contenedor = document.getElementById('contenedor');
 
-/* Funcion de mostrar pokemones*/
-const mostrarPokemones = (data) => {
-  for (let i = 0; i < data.length; i++) {
-    const num = data[i].num;
-    const id = data[i].id;
-    const nombre = data[i].name;
-    const imagen = data[i].img;
+/* Funcion ordenar */
+const ordenar = (condicion) => {
 
-    contenedor.innerHTML += `<div class='poke' name='pokemon' id=${id}>
-    <p> ${num} </p>
-    <p> ${nombre} </p>
-    <img src="${imagen}"/>
-    </div>`;
-  }
-};
-
-const atrapados = document.getElementById('contador');
-let contAtrapados = 0;
-let contNoAtrapados = 0;
-for (let i = 0; i < POKEMON.pokemon.length; i++) {
-  if (POKEMON.pokemon[i].multipliers === null) {
-    contNoAtrapados++;
-  } else {
-    contAtrapados = 148 - contNoAtrapados;
+  /* Ordenar A-Z */
+  if (condicion == 'az') {
+    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+      if (primero.name > segundo.name) {
+        return 1;
+      }
+      if (primero.name < segundo.name) {
+        return -1;
+      }
+      return 0;
+    });
+    mostrarPokemones(pokeOrdenados);
   }
 
-  atrapados.innerHTML = `<div>
-     ${contAtrapados}    ${contNoAtrapados}
-     </div>`;
+  /* Ordenar Z-A */
+  else if (condicion == 'za') {
+    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+      if (primero.name < segundo.name) {
+        return 1;
+      }
+      if (primero.name > segundo.name) {
+        return -1;
+      }
+      return 0;
+    });
+    mostrarPokemones(pokeOrdenados);
+  }
+
+  /* Ordenar aparece mas */
+  else if (condicion == 'spawnmas') {
+    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+      return segundo.spawn_chance - primero.spawn_chance;
+    });
+    mostrarPokemones(pokeOrdenados);
+  }
+
+  /* Ordenar aparece menos */
+  else if (condicion == 'spawnmenos') {
+    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+      return primero.spawn_chance - segundo.spawn_chance;
+    });
+    mostrarPokemones(pokeOrdenados);
+  }
 };
-
-/* cerrar Modal */
- document.getElementById('close').addEventListener('click', () => {
- document.getElementById('my-modal').classList.add('hide');
-});
-
-/* Obtengo todo el contenedor donde estan los pokemones y agrego un evento clik*/
-document.getElementById('contenedor').addEventListener('click',()=>{
-/* Capturo el Id del pokemon que realizo evento en variable numero, se obtine el id del padre
-y le quito 1 para que coincida con el array */
-const numero = parseInt(event.target.parentElement.id)-1;
-/* Pongo condicional que si el atributo name  del padre de ese elemento es pokemon, muestra modal e inserta datos del pokemon*/
-if (event.target.parentElement.getAttribute('name') === 'pokemon') {
-  /* Muestra modal*/
-document.getElementById('my-modal').classList.remove('hide');
-/* Insertar info de pokemon en Modal*/
- document.getElementById('modal-info').innerHTML = `
-<img class="imagenModal" src="${POKEMON.pokemon[numero].img}"/>
-<p> Nombre:  ${POKEMON.pokemon[numero].name}</p>
- <p>Peso: ${POKEMON.pokemon[numero].weight}   </p> 
-<p>Altura: ${POKEMON.pokemon[numero].height}</p>    
-<p>Tipo: ${POKEMON.pokemon[numero].type}</p> `;
-}
-});
 
