2158ff0edd7969292dd713062faf917fbdc07daf
HIDUBA <lescatap@gmail.com>
2019-07-12 08:23:45 -0500

    Agregando porcentajes a huevos

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 /* Funcion ordenar */
 const ordenar = (condicion) => {
   if (condicion === 'az') {
-    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+    const pokeOrdenados = pokedata.sort((primero, segundo) => {
       if (primero.name > segundo.name) {
         return 1;
       }
@@ -12,7 +12,7 @@ const ordenar = (condicion) => {
     });
     return pokeOrdenados;
   } else if (condicion === 'za') {
-    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+    const pokeOrdenados = pokedata.sort((primero, segundo) => {
       if (primero.name < segundo.name) {
         return 1;
       }
@@ -23,17 +23,17 @@ const ordenar = (condicion) => {
     });
     return pokeOrdenados;
   } else if (condicion === 'spawnmas') {
-    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+    const pokeOrdenados = pokedata.sort((primero, segundo) => {
       return segundo.spawn_chance - primero.spawn_chance;
     });
     return pokeOrdenados;
   } else if (condicion === 'spawnmenos') {
-    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+    const pokeOrdenados = pokedata.sort((primero, segundo) => {
       return primero.spawn_chance - segundo.spawn_chance;
     });
     return pokeOrdenados;
   } else if (condicion === 'pokedex') {
-    const pokeOrdenados = POKEMON.pokemon.sort((primero, segundo) => {
+    const pokeOrdenados = pokedata.sort((primero, segundo) => {
       return primero.num - segundo.num;
     });
     return pokeOrdenados;
@@ -41,24 +41,24 @@ const ordenar = (condicion) => {
 };
 
 /* Funcion filtrar tipo */
-const filtrar = (filtroSeleccionado) => {
-  const PokeFiltrados = POKEMON.pokemon.filter((condicion) => {
+const filtrar = (filtroSeleccionado, pokedata) => {
+  const PokeFiltrados = pokedata.filter((condicion) => {
     return condicion.type.indexOf(filtroSeleccionado) > -1;
   });
   return PokeFiltrados;
 };
 
 /* Funcion filtrar debilidad */
-const debilidad = (filtroDebilidad) => {
-  const PokeDebilidad = POKEMON.pokemon.filter((condicion) => {
+const debilidad = (filtroDebilidad, pokedata) => {
+  const PokeDebilidad = pokedata.filter((condicion) => {
     return condicion.weaknesses.indexOf(filtroDebilidad) > -1;
   });
   return PokeDebilidad;
 };
 
 /* Funcion filtrar huevos */
-const huevos = (filtroHuevos) => {
-  const PokeHuevos = POKEMON.pokemon.filter((condicion) => {
+const huevos = (filtroHuevos, pokedata) => {
+  const PokeHuevos = pokedata.filter((condicion) => {
     return condicion.egg.indexOf(filtroHuevos) > -1;
   });
   return PokeHuevos;
